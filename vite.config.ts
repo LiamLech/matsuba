86745fa57531a90ee31faf101a58a7dcebdd192f
176164934+LiamLech@users.noreply.github.com <176164934+LiamLech@users.noreply.github.com>
2026-07-02 16:42:36 +0900

    vite base path修正

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,9 @@
-import { defineConfig } from 'vite'
+﻿import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import { resolve } from 'path'
-
 export default defineConfig({
   plugins: [react()],
+  base: '/matsuba/',
   resolve: {
     alias: {
       '@': resolve(__dirname, './src'),
